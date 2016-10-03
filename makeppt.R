0e7ad74e55f33bf1d86b171daf38bf677794eb75
Brooke Watson <watson@ecohealthalliance.org>
2016-10-03 16:02:30 -0400

    Small changes

--- makeppt.R
+++ makeppt.R
@@ -1,3 +1,10 @@
+######################################################################################### 
+####                    GVP PPTs
+######################################################################################### 
+
+ 
+db <- read.csv("~/Dropbox (EHA)/gvp (1)/notes/GVP mammal species.csv", stringsAsFactors = FALSE)
+ 
 makeppt <- function(data, row){ 
   
   #install required libraries
@@ -8,15 +15,11 @@ makeppt <- function(data, row){
   library(ReporteRs)
   library(foreign)
   #select the country 
-  vars <- c("country", "nmam", "nbirds", "hsreg", "biodivreg", "nzoo")  
-  # easier: vars <- colnames(db)
-  
-  country <- data[row, "country"]
-  nmam <- data[row, "nmam"]
-  nbirds <- data[row, "nbirds"]
-  hsreg <- data[row, "hsreg"] 
-  biodivreg <- data[row, "biodivreg"]
-  nzoo <- data[row, "nzoo"]
+     
+  country <- data[row, 1]
+  nmam <- data[row, 3]
+  # nbirds <- data[row, "nbirds"]
+ 
   
   #define the values in the bullet points that will vary by country 
   
@@ -24,34 +27,50 @@ makeppt <- function(data, row){
   #############################     MAKE BULLET POINTS     #############################
   
   #make the bullets for the first slide 
-  mb1 <- sprintf("%s Mammal species reside in %s; along with %s species of waterbirds", nmam, country, nbirds)
-  mb2 <- "Many are migratory or occur in other countries"
-  mb3 <- sprintf("Densest areas of biodiversity located in the %s", biodivreg)
+  mb1 <- sprintf("%s Mammal species reside in %s", nmam, country)
+  mb2 <- "Many species also occur in other countries"
+  mb3 <- sprintf("Densest areas of mammal biodiversity are located in the red areas, but some areas of low biodiversity are highly abundant")
   mambullets <- c(mb1, "", mb2, "", mb3) 
+  
+  birdbullets <- c("~40 species of Anatidae spp reside throughout India, of roughly 140 species worldwide", "", 
+                    "Migratory birds in the Anatidae family have been implicated in the spread of pandemic influenza")
   
   #make the bullets for the second slide 
   hsbullets <- c("Mammal biodiversity, population density, and land use patterns are key drivers in emergence of infectious diseases","",
-                 sprintf("Risk is elevated across much of %s, particularly in the %s", country, hsreg))
+                 sprintf("Risk is elevated across much of %s, particularly in the red regions", country))
   
   #make the bullets for the third slide 
   zoobullets <- c("This identifies hotspots where we would find the most new zoonotic viruses if we sampled uniformly all mammals","",
-                  sprintf("Over %s unidentified wildlife zoonoses predicted in %s based on modelled analysis (Olival et al. in review, Nature)", nzoo, country),
-                  "", "Only a subset of mammals studied – actual number of viruses could be higher")
-  #define the values before 
+                  sprintf("Over 200 unidentified wildlife zoonoses predicted in %s based on modelled analysis (Olival et al. in review, Nature)", country)) #,
+                  #"", "Only a subset of mammals studied – actual number of viruses could be higher")
+  #last bullet commented out because it was too long. 
   
-  doc = pptx(sprintf("%s GVP test.pptx", country), template = "GVPmaptemp.pptx")
+  doc = pptx(sprintf("%s GVP maps.pptx", country), template = "GVPmaptemp.pptx")
   doc <- doc %>% 
     addSlide( slide.layout = "Custom_for_Maps" ) %>% 
     addTitle(paste0(country, "- Mammal Biodiversity")) %>% 
     addParagraph((value = mambullets )) %>% 
-    addImage("Mammal_biodiversity_FINAL.jpg", width=4.4, height=5.2) %>% 
+ #   addImage(paste0(country, "- Mammal Biodiversity.jpg"), width=4.4, height=5.2) %>% 
+    addSlide( slide.layout = "Custom_for_Maps" ) %>% 
+    addTitle(paste0(country, "- Anatidae Biodiversity")) %>% 
+    addParagraph((value = birdbullets )) %>% 
+ #   addImage(paste0(country, "- Anatidae Biodiversity.jpg"), width=4.4, height=5.2) %>% 
     addSlide(slide.layout = "Custom_for_Maps" ) %>% 
     addTitle(paste0(country, "- EID Hotspots")) %>% 
     addParagraph((value = hsbullets)) %>% 
-    addImage("India_hotspots1-2.5_FINAL.jpg", width=4.4, height=5.2) %>% 
+ #   addImage(paste0(country, "- EID Hotspots.jpg"), width=4.4, height=5.2) %>% 
     addSlide( slide.layout = "Custom_for_Maps" ) %>% 
     addTitle(paste0(country, " - Missing Zoonoses")) %>% 
     addParagraph((value = zoobullets)) %>% 
-    addImage("Missing_Zoonoses_FINAL.jpg", width=4.4, height=5.2) %>% 
+ #   addImage(paste0(country, "Missing_Zoonoses.jpg"), width=4.4, height=5.2) %>% 
     writeDoc(paste0(country, " GVP map.pptx"))   
 }
+
+lapply(db[1:34,], makeppt, db) 
+
+for (n in 6:34) {
+
+makeppt(db, n)
+
+}
+lapply(c(3:5), makeppt, db)
